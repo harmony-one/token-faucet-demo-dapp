344da0972ce8bf0f03fa2c4d42837225f25662c9
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-11-11 15:12:35 +0100

    Update deployment

--- ui/src/config/mainnet.config.js
+++ ui/src/config/mainnet.config.js
@@ -1,6 +1,6 @@
 const config = {
   network: 'mainnet',
-  addresses: {"token": "0x6746915e8191708e2355566f0f4a3c0ab5573ac5", "faucet": "0xd88d6547e971cb1b8eb5e869b05b923c9c2d9c5c"}
+  addresses: {"token": "0xddd35ef64ee319580a8e7ae6b8f5edd985a081ad", "faucet": "0x00fd90158f857f1e85f0a043179445623836f6d8"}
 };
 
 export default config;
